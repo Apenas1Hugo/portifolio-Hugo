49d6d5e71f8ee3704f700f3ae27bea43a1f55356
Apenas1Hugo <hugorodriguesm27@gmail.com>
2025-11-24 16:20:03 -0300

    Update projetos
    novos projetos adicionados e algumas modificações visuais.

--- src/components/ProjetosGit.jsx
+++ src/components/ProjetosGit.jsx
@@ -1,13 +1,40 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaHtml5, FaCss3Alt, FaJs, FaReact } from "react-icons/fa"; //icones
+import { SiTailwindcss, SiVite } from "react-icons/si";
+import { LuNetwork } from "react-icons/lu";
+
+const techIcons = {
+  html: <FaHtml5 size={30} className="text-orange-500" />,
+  css: <FaCss3Alt size={30} className="text-blue-500" />,
+  javascript: <FaJs size={30} className="text-yellow-400" />,
+  react: <FaReact size={30} className="text-cyan-400" />,
+  tailwind: <SiTailwindcss size={30} className="text-sky-400" />,
+  vite: <SiVite size={30} className="text-purple-400" />,
+  api: <LuNetwork size={30} className="text-green-400" />,
+};
 
 const projects = [
   {
     id: 1,
-    img: "./img/todolist.png",
     titulo: "To-do-list",
-    texto: "Um To-do list simples criada com html,css e JavaScript",
+    texto: "Um To-do list simples criada com HTML, CSS e JavaScript",
     link: "https://github.com/Apenas1Hugo/To-do-list",
+    tech: ["html", "css", "javascript"],
+  },
+  {
+    id: 2,
+    titulo: "Portifólio",
+    texto: "Simples portifólio criado usando React e Tailwind",
+    link: "https://github.com/Apenas1Hugo/portifolio-Hugo",
+    tech: ["react", "tailwind", "vite"],
+  },
+  {
+    id: 3,
+    titulo: "MovieSearch",
+    texto:
+      "Interface web interativa construída com React que permite ao usuário pesquisar filmes rapidamente usando dados da API do The Movie Database.",
+    link: "https://github.com/Apenas1Hugo/MovieSearch-TMDB",
+    tech: ["react", "css", "vite", "api"],
   },
 ];
 
@@ -27,22 +54,35 @@ export default function Projetos() {
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {projects.map(({ id, img, titulo, texto, link }) => (
+        {projects.map(({ id, titulo, texto, link, tech }) => (
           <div
             key={id}
             className="
               flex flex-col
               bg-[rgb(26,40,56)]
-              rounded-lg overflow-hidden
-              shadow-lg
+              rounded-lg shadow-lg overflow-visible
             "
           >
-            <div className="w-full overflow-hidden">
-              <img
-                src={img}
-                alt={titulo}
-                className="w-full h-auto object-cover"
-              />
+            {/* Area dos ícones */}
+            <div className="flex gap-4 justify-center items-center py-6 bg-[rgb(45,61,78)]">
+              {tech.map((t) => (
+                <div key={t} className="relative group">
+                  {techIcons[t]}
+
+                  <span
+                    className="
+                      absolute left-1/2 -translate-x-1/2 -top-8
+                      bg-black text-white text-xs px-2 py-1 rounded
+                      opacity-0 group-hover:opacity-100
+                      transition-opacity
+                      pointer-events-none
+                      whitespace-nowrap
+      "
+                  >
+                    {t.toUpperCase()}
+                  </span>
+                </div>
+              ))}
             </div>
 
             <div className="p-4 flex flex-col flex-grow">
@@ -62,7 +102,6 @@ export default function Projetos() {
                   text-[rgb(224,225,221)]
                   px-4 py-2 rounded
                   transition-colors
-                
                 "
               >
                 <FaGithub className="mr-2" />
